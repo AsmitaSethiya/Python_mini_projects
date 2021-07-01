00e0dd66eb7c94442d887089c6571ee4e1e2b492
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-07-02 00:25:18 +0530

    Create Quiz_Game.py

---
+++ Quiz_Game.py
@@ -0,0 +1,61 @@
+print("Welcome to my Computer Quiz!")
+
+playing = input("do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play :)")
+
+score = 0
+
+answer = input("What does CPU stands for? ")
+
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stands for? ")
+
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+
+answer = input("What does RAM stands for? ")
+
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does WWW stads for? ")
+if answer.lower() == "world wide web":
+    print("Correct!")
+    score +=1
+else:
+    print("Incorrect!")
+
+
+answer = input("What is the full form of Email ? ")
+
+if answer.lower() == "electronic mail":
+    print("Correct!")
+    score +=1
+else:
+    print("Incorrect!")    
+
+
+print("You got " + str(score) + " questions correct!") 
+
+print("You got " + str((score / 5) * 100) + "%.")
+
+
+
+
+
